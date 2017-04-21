eb0c463b6817504819857595d47a71544d180a43
Offir Golan <offirgolan@gmail.com>
2017-04-21 11:02:16 -0700

    More test setup!

---
+++ tests/unit/query-params-builder-test.js
@@ -0,0 +1,10 @@
+import QueryParamsBuilder from 'ember-parachute/-private/query-params-builder';
+import { module, test } from 'qunit';
+
+module('Unit | Query Params Builder');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = new QueryParamsBuilder();
+  assert.ok(result);
+});
---
+++ tests/unit/transforms-test.js
@@ -0,0 +1,10 @@
+import Transforms from 'ember-parachute/-private/transforms';
+import { module, test } from 'qunit';
+
+module('Unit | Transforms');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = Transforms.Boolean('true');
+  assert.equal(result, true);
+});
