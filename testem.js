af559d06785a146ad12a578573a69794a10b8b7e
Offir Golan <offirgolan@gmail.com>
2017-04-21 13:51:57 -0700

    Fix test cases
    Remove use of includes since phantomjs is hella dumb

--- testem.js
+++ testem.js
@@ -3,9 +3,10 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "Chrome"
+    "PhantomJS"
   ],
   "launch_in_dev": [
+    "PhantomJS",
     "Chrome"
   ]
 };
